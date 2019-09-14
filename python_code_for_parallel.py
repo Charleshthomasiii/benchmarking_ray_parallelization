ae6551c20dc3bd57614f20c8dd27c17fd39055e2
charleshthomasiii <charlie.h.thomas@gmail.com>
2019-09-14 16:45:58 -0400

    formatting

--- python_code_for_parallel.py
+++ python_code_for_parallel.py
@@ -1,7 +1,6 @@
 import ray
-import random
 import time
- 
+import random
 def timerfunc(func):
     """
     A timer decorator
@@ -32,40 +31,38 @@ class CoreUnit():
 
     def generate_sum(self):
         for i in self.values:
-        	self.sum += i
+            self.sum += i
     def return_sum(self):
-    	return self.sum
+        return self.sum
 
 @timerfunc
 def setup_cores(integers, cores):
-	#need to split integers into even number of lists
-	partition_length = len(integers)//cores
-	remainder = len(integers)%cores
-	#feed each core a partition
-	counters = []
-	for core_num in range(cores):
-		#pass in section of integers with size of partition length for each core, then add remainder to last one
-		partition=[]
-		for offset in range(partition_length):
-			partition.append(integers[core_num*partition_length + offset])
-		if remainder != 0 and core_num==cores-1:
-			for x in range(remainder):
-				partition.append(integers[(cores)*partition_length+x])
-		counters.append(CoreUnit.remote(partition))
-	return counters
+    partition_length = len(integers)//cores
+    remainder = len(integers)%cores
+    counters = []
+    for core_num in range(cores):
+        partition=integers[core_num*partition_length:core_num*partition_length+partition_length]
+        # print(partition)
+       # for offset in range(partition_length):
+        # partition.append(integers[core_num*partition_length + offset])
+        if remainder != 0 and core_num==cores-1:
+            for x in range(remainder):
+                partition.append(integers[(cores)*partition_length+x])
+        counters.append(CoreUnit.remote(partition))
+    return counters
 
 @timerfunc
 def compute_sum(counters):
-	for c in counters:
-		c.generate_sum.remote()
-	results = [c.return_sum.remote() for c in counters]		
-	print(ray.get(results))
+    for c in counters:
+        c.generate_sum.remote()
+    results = [c.return_sum.remote() for c in counters]     
+    print(ray.get(results))
 
 
-array =[1]*100000000
+array =[random.randint(0,100)]*10000000
 for x in range(4):
-	print('----------------calculating sum for {} cores----------------'.format(x+1))
-	counters = setup_cores(array,x+1)
-	compute_sum(counters)
-	# counters = [CoreUnit.remote([1,2,3,4]) for i in range(cores)]
+    print('----------------calculating sum for {} cores----------------'.format(x+1))
+    counters = setup_cores(array,x+1)
+    compute_sum(counters)
+    # counters = [CoreUnit.remote([1,2,3,4]) for i in range(cores)]
 # counters = [CoreUnit.remote([1,2,3,4]) for i in range(4)]
